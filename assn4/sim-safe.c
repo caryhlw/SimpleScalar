081636192803087551d4592127ff16802492b398
Cary <melstrom@gmail.com>
2013-11-29 21:40:18 -0800

    4-way set associative cache

--- assn4/sim-safe.c
+++ assn4/sim-safe.c
@@ -48,7 +48,6 @@
  * Copyright (C) 1994-2003 by Todd M. Austin, Ph.D. and SimpleScalar, LLC.
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -69,17 +68,19 @@ static counter_t g_icache_miss;
 
 struct block
 {
-	int			m_valid;
-	md_addr_t	m_tag;
+	int m_valid;
+	md_addr_t m_tag;
+	counter_t m_timestamp;
 };
 
 struct cache
 {
-	struct block	*m_tag_array;
-	unsigned		m_total_blocks;
-	unsigned		m_set_shift;
-	unsigned		m_set_mask;
-	unsigned		m_tag_shift;
+	struct block *m_tag_array;
+	unsigned m_total_blocks;
+	unsigned m_set_shift;
+	unsigned m_set_mask;
+	unsigned m_tag_shift;
+	unsigned m_nways;
 };
 /*
  * This file implements a functional simulator.  This functional simulator is
@@ -101,104 +102,91 @@ static counter_t sim_num_refs = 0;
 static unsigned int max_insts;
 
 /* register simulator-specific options */
-void
-sim_reg_options(struct opt_odb_t *odb)
+void sim_reg_options(struct opt_odb_t *odb)
 {
-  opt_reg_header(odb, 
-"sim-safe: This simulator implements a functional simulator.  This\n"
-"functional simulator is the simplest, most user-friendly simulator in the\n"
-"simplescalar tool set.  Unlike sim-fast, this functional simulator checks\n"
-"for all instruction errors, and the implementation is crafted for clarity\n"
-"rather than speed.\n"
-		 );
+	opt_reg_header(odb,
+			"sim-safe: This simulator implements a functional simulator.  This\n"
+					"functional simulator is the simplest, most user-friendly simulator in the\n"
+					"simplescalar tool set.  Unlike sim-fast, this functional simulator checks\n"
+					"for all instruction errors, and the implementation is crafted for clarity\n"
+					"rather than speed.\n");
 
-  /* instruction limit */
-  opt_reg_uint(odb, "-max:inst", "maximum number of inst's to execute",
-	       &max_insts, /* default */0,
-	       /* print */TRUE, /* format */NULL);
+	/* instruction limit */
+	opt_reg_uint(odb, "-max:inst", "maximum number of inst's to execute",
+			&max_insts, /* default */0,
+			/* print */TRUE, /* format */NULL);
 
 }
 
 /* check simulator-specific option values */
-void
-sim_check_options(struct opt_odb_t *odb, int argc, char **argv)
+void sim_check_options(struct opt_odb_t *odb, int argc, char **argv)
 {
-  /* nada */
+	/* nada */
 }
 
 /* register simulator-specific statistics */
-void
-sim_reg_stats(struct stat_sdb_t *sdb)
+void sim_reg_stats(struct stat_sdb_t *sdb)
 {
-  stat_reg_counter(sdb, "sim_num_insn",
-		   "total number of instructions executed",
-		   &sim_num_insn, sim_num_insn, NULL);
-  stat_reg_counter(sdb, "sim_num_refs",
-		   "total number of loads and stores executed",
-		   &sim_num_refs, 0, NULL);
-  stat_reg_int(sdb, "sim_elapsed_time",
-	       "total simulation time in seconds",
-	       &sim_elapsed_time, 0, NULL);
-  stat_reg_formula(sdb, "sim_inst_rate",
-		   "simulation speed (in insts/sec)",
-		   "sim_num_insn / sim_elapsed_time", NULL);
-  ld_reg_stats(sdb);
-  mem_reg_stats(mem, sdb);
+	stat_reg_counter(sdb, "sim_num_insn",
+			"total number of instructions executed", &sim_num_insn,
+			sim_num_insn, NULL);
+	stat_reg_counter(sdb, "sim_num_refs",
+			"total number of loads and stores executed", &sim_num_refs, 0,
+			NULL);
+	stat_reg_int(sdb, "sim_elapsed_time", "total simulation time in seconds",
+			&sim_elapsed_time, 0, NULL);
+	stat_reg_formula(sdb, "sim_inst_rate", "simulation speed (in insts/sec)",
+			"sim_num_insn / sim_elapsed_time", NULL);
+	ld_reg_stats(sdb);
+	mem_reg_stats(mem, sdb);
 
-
-  stat_reg_counter(sdb, "sim_num_icache_miss",
-              "total number of instruction cache misses",
-              &g_icache_miss, 0, NULL);
-  stat_reg_formula(sdb, "sim_icache_miss_rate",
-              "instruction cache miss rate (percentage)",
-              "100*(sim_num_icache_miss / sim_num_insn)", NULL);
+	stat_reg_counter(sdb, "sim_num_icache_miss",
+			"total number of instruction cache misses", &g_icache_miss, 0,
+			NULL);
+	stat_reg_formula(sdb, "sim_icache_miss_rate",
+			"instruction cache miss rate (percentage)",
+			"100*(sim_num_icache_miss / sim_num_insn)", NULL);
 }
 
 /* initialize the simulator */
-void
-sim_init(void)
+void sim_init(void)
 {
-  sim_num_refs = 0;
+	sim_num_refs = 0;
 
-  /* allocate and initialize register file */
-  regs_init(&regs);
+	/* allocate and initialize register file */
+	regs_init(&regs);
 
-  /* allocate and initialize memory space */
-  mem = mem_create("mem");
-  mem_init(mem);
+	/* allocate and initialize memory space */
+	mem = mem_create("mem");
+	mem_init(mem);
 }
 
 /* load program into simulated state */
-void
-sim_load_prog(char *fname,		/* program to load */
-	      int argc, char **argv,	/* program arguments */
-	      char **envp)		/* program environment */
+void sim_load_prog(char *fname, /* program to load */
+int argc, char **argv, /* program arguments */
+char **envp) /* program environment */
 {
-  /* load program text and data, set up environment, memory, and regs */
-  ld_load_prog(fname, argc, argv, envp, &regs, mem, TRUE);
+	/* load program text and data, set up environment, memory, and regs */
+	ld_load_prog(fname, argc, argv, envp, &regs, mem, TRUE);
 }
 
 /* print simulator-specific configuration information */
-void
-sim_aux_config(FILE *stream)		/* output stream */
+void sim_aux_config(FILE *stream) /* output stream */
 {
-  /* nothing currently */
+	/* nothing currently */
 }
 
 /* dump simulator-specific auxiliary simulator statistics */
-void
-sim_aux_stats(FILE *stream)		/* output stream */
+void sim_aux_stats(FILE *stream) /* output stream */
 {
-  /* nada */
+	/* nada */
 }
 
 /* un-initialize simulator-specific state */
-void
-sim_uninit(void)
+void sim_uninit(void)
 {
-  /* nada */
+	/* nada */
 }
-
 
 /*
  * configure the execution engine
@@ -300,67 +288,86 @@ sim_uninit(void)
 void cache_access(struct cache *c, unsigned addr, counter_t *miss_counter)
 {
 	unsigned index, tag;
+	int i, k;
+	unsigned int min = max_insts;
 	index = (addr >> c->m_set_shift) & c->m_set_mask;
 	tag = (addr >> c->m_tag_shift);
 	assert(index < c->m_total_blocks);
-	if (!(c->m_tag_array[index].m_valid && (c->m_tag_array[index].m_tag == tag)))
+	for (i = 0; i < c->m_nways; i++)
 	{
-		*miss_counter = *miss_counter + 1;
-		c->m_tag_array[index].m_valid = 1;
-		c->m_tag_array[index].m_tag = tag;
+		if ((c->m_tag_array[index + i].m_valid
+				&& (c->m_tag_array[index + i].m_tag == tag)))
+		{
+			(&(c->m_tag_array[index + i]))->m_timestamp = sim_num_insn;
+			break;
+		}
+
+		else
+		{
+			if ((&(c->m_tag_array[index + i]))->m_timestamp < min)
+			{
+				k = i;
+			}
+			if (i == (c->m_nways - 1))
+			{
+				*miss_counter = *miss_counter + 1;
+				c->m_tag_array[index + k].m_valid = 1;
+				c->m_tag_array[index + k].m_tag = tag;
+			}
+		}
 	}
 }
 
 /* start simulation, program loaded, processor precise state initialized */
-void
-sim_main(void)
+void sim_main(void)
 {
-  md_inst_t inst;
-  register md_addr_t addr;
-  enum md_opcode op;
-  register int is_write;
-  enum md_fault_type fault;
+	md_inst_t inst;
+	register md_addr_t addr;
+	enum md_opcode op;
+	register int is_write;
+	enum md_fault_type fault;
 
-  struct cache *icache = (struct cache *) calloc( sizeof(struct cache), 1 );
-  icache->m_tag_array = (struct block *) calloc( sizeof(struct block), 512 );
-  icache->m_total_blocks = 512;
-  icache->m_set_shift = 6;
-  icache->m_set_mask = (1<<9)-1;
-  icache->m_tag_shift = 15;
+	struct cache *icache = (struct cache *) calloc(sizeof(struct cache), 1);
+	icache->m_tag_array = (struct block *) calloc(sizeof(struct block), 1024);
+	icache->m_total_blocks = 1024;
+	icache->m_set_shift = 5;
+	icache->m_set_mask = (1 << 10) - 1;
+	icache->m_tag_shift = 15;
+	icache->m_nways = 4;
 
-  fprintf(stderr, "sim: ** starting functional simulation **\n");
+	fprintf(stderr, "sim: ** starting functional simulation **\n");
 
-  /* set up initial default next PC */
-  regs.regs_NPC = regs.regs_PC + sizeof(md_inst_t);
+	/* set up initial default next PC */
+	regs.regs_NPC = regs.regs_PC + sizeof(md_inst_t);
 
-
-  while (TRUE)
-    {
-      /* maintain $r0 semantics */
-      regs.regs_R[MD_REG_ZERO] = 0;
+	while (TRUE)
+	{
+		/* maintain $r0 semantics */
+		regs.regs_R[MD_REG_ZERO] = 0;
 #ifdef TARGET_ALPHA
-      regs.regs_F.d[MD_REG_ZERO] = 0.0;
+		regs.regs_F.d[MD_REG_ZERO] = 0.0;
 #endif /* TARGET_ALPHA */
 
-      cache_access(icache, regs.regs_PC, &g_icache_miss);
-      /* get the next instruction to execute */
-      MD_FETCH_INST(inst, mem, regs.regs_PC);
+		cache_access(icache, regs.regs_PC, &g_icache_miss);
+		/* get the next instruction to execute */
+		MD_FETCH_INST(inst, mem, regs.regs_PC);
 
-      /* keep an instruction count */
-      sim_num_insn++;
+		/* keep an instruction count */
+		sim_num_insn++;
 
-      /* set default reference address and access mode */
-      addr = 0; is_write = FALSE;
+		/* set default reference address and access mode */
+		addr = 0;
+		is_write = FALSE;
 
-      /* set default fault - none */
-      fault = md_fault_none;
+		/* set default fault - none */
+		fault = md_fault_none;
 
-      /* decode the instruction */
-      MD_SET_OPCODE(op, inst);
+		/* decode the instruction */
+		MD_SET_OPCODE(op, inst);
 
-      /* execute the instruction */
-      switch (op)
-	{
+		/* execute the instruction */
+		switch (op)
+		{
 #define DEFINST(OP,MSK,NAME,OPFORM,RES,FLAGS,O1,O2,I1,I2,I3)		\
 	case OP:							\
           SYMCAT(OP,_IMPL);						\
@@ -372,38 +379,37 @@ sim_main(void)
 #define DECLARE_FAULT(FAULT)						\
 	  { fault = (FAULT); break; }
 #include "machine.def"
-	default:
-	  panic("attempted to execute a bogus opcode");
-      }
+		default:
+			panic("attempted to execute a bogus opcode");
+		}
 
-      if (fault != md_fault_none)
-	fatal("fault (%d) detected @ 0x%08p", fault, regs.regs_PC);
+		if (fault != md_fault_none)
+			fatal("fault (%d) detected @ 0x%08p", fault, regs.regs_PC);
 
-      if (verbose)
-	{
-	  myfprintf(stderr, "%10n [xor: 0x%08x] @ 0x%08p: ",
-		    sim_num_insn, md_xor_regs(&regs), regs.regs_PC);
-	  md_print_insn(inst, regs.regs_PC, stderr);
-	  if (MD_OP_FLAGS(op) & F_MEM)
-	    myfprintf(stderr, "  mem: 0x%08p", addr);
-	  fprintf(stderr, "\n");
-	  /* fflush(stderr); */
+		if (verbose)
+		{
+			myfprintf(stderr, "%10n [xor: 0x%08x] @ 0x%08p: ", sim_num_insn,
+					md_xor_regs(&regs), regs.regs_PC);
+			md_print_insn(inst, regs.regs_PC, stderr);
+			if (MD_OP_FLAGS(op) & F_MEM)
+				myfprintf(stderr, "  mem: 0x%08p", addr);
+			fprintf(stderr, "\n");
+			/* fflush(stderr); */
+		}
+
+		if (MD_OP_FLAGS(op) & F_MEM)
+		{
+			sim_num_refs++;
+			if (MD_OP_FLAGS(op) & F_STORE)
+				is_write = TRUE;
+		}
+
+		/* go to the next instruction */
+		regs.regs_PC = regs.regs_NPC;
+		regs.regs_NPC += sizeof(md_inst_t);
+
+		/* finish early? */
+		if (max_insts && sim_num_insn >= max_insts)
+			return;
 	}
-
-      if (MD_OP_FLAGS(op) & F_MEM)
-	{
-	  sim_num_refs++;
-	  if (MD_OP_FLAGS(op) & F_STORE)
-	    is_write = TRUE;
-	}
-
-
-      /* go to the next instruction */
-      regs.regs_PC = regs.regs_NPC;
-      regs.regs_NPC += sizeof(md_inst_t);
-
-      /* finish early? */
-      if (max_insts && sim_num_insn >= max_insts)
-	return;
-    }
 }
